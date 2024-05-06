1e4ae35f3c902cc7340c5b4c2042f86d54c8f37f
vadim228sokol <vadim228sokol@gmail.com>
2024-05-06 14:00:12 +0300

    commit 2

---
+++ app_blog/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+from .models import ArticleImage
+
+
+class ArticleImageForm(forms.ModelForm):
+    image = forms.ImageField(
+        widget=forms.ClearableFileInput(
+            attrs={'multiple': True}))
+
+    class Meta:
+        model = ArticleImage
+        fields = '__all__'
